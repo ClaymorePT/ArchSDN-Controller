b554f89d0b734909c3095a6805d353414cbc053d
Carlos Miguel Ferreira <carlosmf.pt@gmail.com>
2018-07-31 18:04:25 +0100

    Refactor OF Packet-In event handlers
    This commit marks the beginning of a series of commits to refactor the packet-in event
      handling structure.
    Until now, the event-handling code for all kinds of packets was implemented within the
      same function. This commit segregates the archsdn L2 packets into an isolated code sheat.
    
    Signed-off-by: Carlos Miguel Ferreira <carlosmf.pt@gmail.com>

--- src/archsdn/engine/datapath_events/packet_in.py
+++ src/archsdn/engine/datapath_events/packet_in.py
@@ -5,7 +5,6 @@ from ipaddress import IPv4Address
 import struct
 from uuid import UUID
 import time
-import random
 
 from scapy.packet import Padding, Raw
 from scapy.layers.l2 import Ether, ARP
@@ -26,6 +25,8 @@ from archsdn.engine import entities
 from archsdn.engine import services
 from archsdn import p2p_requests
 from archsdn.engine.exceptions import PathNotFound
+
+from archsdn.engine.datapath_events.packet_in_events.archsdn_L2 import process_archsdn_control
 
 _log = logging.getLogger(logger_module_name(__file__))
 
@@ -62,91 +63,7 @@ def process_event(packet_in_event):
 
     layer_num = 0
     if pkt_ethertype == 0xAAAA:  # ArchSDN Hello Packet : Ether Type -> 0xAAAA
-        layer_num += 1
-        archsdn_layer = memoryview(pkt.getlayer(layer_num).fields['load'])
-        if len(archsdn_layer) < 2:
-            _log.warning(
-                "ArchSDN Beacon Ignored. Payload length is lower than 2. Got {:d}".format(len(archsdn_layer))
-            )
-            return
-
-        (msg_type,) = struct.unpack("!H", archsdn_layer[0:struct.calcsize("!H")])
-        msg_payload = archsdn_layer[2:]
-        datapath_obj = msg.datapath
-
-        if msg_type == 1:
-            msg_type_1_payload_format = "!16s8s"
-            msg_type_1_payload_len = struct.calcsize(msg_type_1_payload_format)
-            if len(msg_payload) < msg_type_1_payload_len:
-                _log.warning(
-                    "ArchSDN Beacon Ignored. It has invalid size: it's {:d} when it should be at least {:d}".format(
-                        len(msg_payload), msg_type_1_payload_len
-                    )
-                )
-                return
-
-            (sender_controller_uuid_bytes, hash_val_bytes) = struct.unpack(
-                msg_type_1_payload_format, msg_payload[0:msg_type_1_payload_len]
-            )
-
-            hash_val = int.from_bytes(hash_val_bytes, byteorder='big', signed=False)
-            sender_controller_uuid = UUID(bytes=sender_controller_uuid_bytes)
-
-            if controller_uuid != sender_controller_uuid:
-                _log.debug(
-                    "Switch {:016X} received Beacon Packet from a Controller with ID {:s}, with the hash value {:X}"
-                    "".format(
-                        datapath_id, str(sender_controller_uuid), hash_val
-                    )
-                )
-                if not sector.is_entity_registered(sender_controller_uuid):
-                    sector.register_entity(
-                        entities.Sector(
-                            controller_id=sender_controller_uuid
-                        )
-                    )
-                if not sector.is_port_connected(datapath_id, pkt_in_port):
-                    sector.connect_entities(
-                        datapath_id, sender_controller_uuid,
-                        switch_port_no=pkt_in_port,
-                        hash_val=hash_val
-                    )
-
-                    # Activate segregation flow at the switch port for the detected sector
-                    services.sector_segregation_flow_activation(datapath_obj, pkt_in_port)
-
-            else:
-                (sender_datapath_id, sender_port_out) = globals.beacons_hash_table[hash_val]
-                _log.debug(
-                    "Switch {:016X} received Beacon Packet sent by this very own controller with the hash value "
-                    "{:X}".format(
-                        datapath_id, hash_val
-                    )
-                )
-
-                if not sector.is_port_connected(datapath_id, pkt_in_port):
-                    sector.connect_entities(
-                        datapath_id, sender_datapath_id,
-                        switch_a_port_no=pkt_in_port,
-                        switch_b_port_no=sender_port_out
-                    )
-                    # Activate segregation flow at the switch port for the detected sector switch
-                    services.switch_segregation_flow_activation(datapath_obj, pkt_in_port)
-
-                    sender_datapath_obj = globals.get_datapath_obj(sender_datapath_id)
-                    services.switch_segregation_flow_activation(sender_datapath_obj, sender_port_out)
-                    _log.debug(
-                        "Connecting switch {:016X} using port {:d} to switch {:016X} at port {:d}".format(
-                            datapath_id, sender_datapath_id, pkt_in_port, sender_port_out
-                        )
-                    )
-
-        else:
-            _log.warning(
-                "Ignoring ArchSDN Message received at switch {:016X} (Unknown type: {:d})".format(
-                    datapath_id, msg_type
-                )
-            )
+        process_archsdn_control(packet_in_event)
 
     elif pkt.haslayer(ARP):  # Answering to ARP Packet
         layer_num += 1
