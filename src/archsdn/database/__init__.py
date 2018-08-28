ab45170d73526872f922553bbee07e3e81456487
Carlos Miguel Ferreira <carlosmf.pt@gmail.com>
2018-08-28 18:13:54 +0100

    Database code refactoring
    Since the database is no longer implemented in SQLite3, the wrapping code and
      the DB SQL schema were removed.
    
    Signed-off-by: Carlos Miguel Ferreira <carlosmf.pt@gmail.com>

--- src/archsdn/database/__init__.py
+++ src/archsdn/database/__init__.py
@@ -25,146 +25,32 @@ __all__ = [
     "AddressNotRegistered"
 ]
 
-
-import sys
-#import atexit
-import logging
-
-#from threading import Thread
-#from queue import Queue
-#from eventlet.queue import LightQueue as Queue
-#import eventlet
-#from eventlet.event import Event
-#from eventlet import spawn
-
-from archsdn.helpers import logger_module_name
-
-from archsdn.database.internals.generic import initialise as __initialise
-from archsdn.database.internals.generic import infos as __get_database_info
-from archsdn.database.internals.generic import close as __close
-from archsdn.database.internals.datapath import register as __register_datapath
-from archsdn.database.internals.datapath import query_info as __query_datapath_info
-from archsdn.database.internals.datapath import remove as __remove_datapath
-from archsdn.database.internals.datapath import is_registered as __is_datapath_registered
-from archsdn.database.internals.datapath import dump_ids as __dump_datapth_registered_ids
-from archsdn.database.internals.datapath import dump_datapath_clients_ids as __dump_datapth_registered_clients_ids
-from archsdn.database.internals.clients import register as __register_client
-from archsdn.database.internals.clients import query_info as __query_client_info
-from archsdn.database.internals.clients import query_client_id as __query_client_id
-from archsdn.database.internals.clients import query_address_info as __query_address_information
-from archsdn.database.internals.clients import remove as __remove_client
-from archsdn.database.internals.clients import update_addresses as __update_client_addresses
+from archsdn.database.internals.generic import initialise
+from archsdn.database.internals.generic import infos as get_database_info
+from archsdn.database.internals.generic import close
+from archsdn.database.internals.datapath import register as register_datapath
+from archsdn.database.internals.datapath import query_info as query_datapath_info
+from archsdn.database.internals.datapath import remove as remove_datapath
+from archsdn.database.internals.datapath import is_registered as is_datapath_registered
+from archsdn.database.internals.datapath import dump_ids as dump_datapth_registered_ids
+from archsdn.database.internals.datapath import dump_datapath_clients_ids as dump_datapth_registered_clients_ids
+from archsdn.database.internals.clients import register as register_client
+from archsdn.database.internals.clients import query_info as query_client_info
+from archsdn.database.internals.clients import query_client_id as query_client_id
+from archsdn.database.internals.clients import query_address_info
+from archsdn.database.internals.clients import remove as remove_client
+from archsdn.database.internals.clients import update_addresses as update_client_addresses
 
 from archsdn.database.internals.exceptions import \
-    ClientNotRegistered as __ClientNotRegistered, \
-    ClientAlreadyRegistered as __ClientAlreadyRegistered, \
-    DatapathNotRegistered as __DatapathNotRegistered, \
-    DatapathAlreadyRegistered as __DatapathAlreadyRegistered, \
-    FlowAlreadyRegistered as __FlowAlreadyRegistered, \
-    FlowNotRegistered as __FlowNotRegistered, \
-    AddressNotRegistered as __AddressNotRegistered
+    ClientNotRegistered, \
+    ClientAlreadyRegistered, \
+    DatapathNotRegistered, \
+    DatapathAlreadyRegistered, \
+    FlowAlreadyRegistered, \
+    FlowNotRegistered, \
+    AddressNotRegistered
 
 from archsdn.database.volatile_data import \
-    query_volatile_info as __query_volatile_info, \
-    update_volatile_information as __update_volatile_information
+    query_volatile_info as query_volatile_info, \
+    update_volatile_information as update_volatile_information
 
-
-_callbacks = {
-    "initialise": __initialise,
-    "close": __close,
-    "get_database_info": __get_database_info,
-    "register_datapath": __register_datapath,
-    "is_datapath_registered": __is_datapath_registered,
-    "query_datapath_info": __query_datapath_info,
-    "remove_datapath": __remove_datapath,
-    "dump_datapth_registered_ids": __dump_datapth_registered_ids,
-    "dump_datapth_registered_clients_ids": __dump_datapth_registered_clients_ids,
-    "register_client": __register_client,
-    "query_client_info": __query_client_info,
-    "query_client_id": __query_client_id,
-    "query_address_info": __query_address_information,
-    "remove_client": __remove_client,
-    "update_client_addresses": __update_client_addresses,
-    "query_volatile_info": __query_volatile_info,
-    "update_volatile_information": __update_volatile_information
-}
-
-_exceptions = {
-    "ClientNotRegistered": __ClientNotRegistered,
-    "ClientAlreadyRegistered": __ClientAlreadyRegistered,
-    "DatapathNotRegistered": __DatapathNotRegistered,
-    "DatapathAlreadyRegistered": __DatapathAlreadyRegistered,
-    "Flow_AlreadyRegistered": __FlowAlreadyRegistered,
-    "FlowNotRegistered": __FlowNotRegistered,
-    "AddressNotRegistered": __AddressNotRegistered,
-}
-
-_log = logging.getLogger(logger_module_name(__file__))
-#_shutdown = None
-class __Wrapper:
-    def __init__(self, wrapped):
-        global _shutdown
-
-        self.__wrapped = wrapped
-        # self.__pool = eventlet.GreenPool()
-        # self.__shutdown_event = Event()
-        #
-        # def database_thread_main(start_event):
-        #     try:
-        #         start_event.send()
-        #         self.__pool.waitall()
-        #
-        #     except Exception:
-        #         custom_logging_callback(_log, logging.ERROR, *sys.exc_info())
-        #     finally:
-        #         _log.debug("Database thread is shutting down...")
-        #         self.__shutdown_event.send()
-        #
-        # def shutdown():
-        #     self.__pool.
-        #
-        #     self.__funcs_queue.put(None)
-        #     self.__shutdown_event.wait()
-        #
-        # _shutdown = shutdown
-        #
-        # boot_event = Event()
-        # self.__database_thread = Thread(target=database_thread_main, args=(boot_event,))
-        # self.__database_thread.start()
-        # boot_event.wait()
-
-
-    def __getattr__(self, name):
-        if name in _exceptions:
-            return _exceptions[name]
-
-        if name not in _callbacks:
-            raise AttributeError("module has no member called {:s}".format(name))
-
-        def attr(*args, **kwargs):
-            return _callbacks[name](*args, **kwargs)
-            # event = Event()
-            #
-            # def cr(*args, **kwargs):
-            #     try:
-            #         print("5" * 20)
-            #         event.send(_callbacks[name](*args, **kwargs))
-            #         print("6" * 20)
-            #
-            #     except Exception:
-            #         event.send_exception(*sys.exc_info())
-            #
-            # print("1" * 20)
-            # self.__pool.spawn(cr)
-            # print("4" * 20)
-            # result = event.wait()
-            # print("7" * 20)
-            # print(result)
-            # return result
-
-        return attr
-
-
-sys.modules[__name__] = __Wrapper(sys.modules[__name__])
-#assert _shutdown, "_shutdown is None. This means the Wrapper was not initialized."
-#atexit.register(_shutdown)
